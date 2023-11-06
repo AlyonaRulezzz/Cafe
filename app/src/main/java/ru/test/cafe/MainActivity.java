262b9101beb34f738b38539c86dbd6900b1389ec
milkfist <milkfist@student.21-school.ru>
2023-11-07 02:07:40 +0300

    added xml of 3rd screen

--- app/src/main/java/ru/test/cafe/MainActivity.java
+++ app/src/main/java/ru/test/cafe/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, OrderActivity.class);
+        Intent intent = new Intent(this, OrderDetailsActivity.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/ru/test/cafe/OrderDetailsActivity.java
@@ -0,0 +1,14 @@
+package ru.test.cafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+    }
+}
