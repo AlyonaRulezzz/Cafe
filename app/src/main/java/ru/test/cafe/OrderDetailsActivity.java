689c41f8f01395af0b8ad9740c5b30c20961d84c
milkfist <milkfist@student.21-school.ru>
2023-11-08 18:36:18 +0300

    added logic on 3rd screen

--- app/src/main/java/ru/test/cafe/OrderDetailsActivity.java
+++ app/src/main/java/ru/test/cafe/OrderDetailsActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +13,37 @@ import java.util.List;
 public class OrderDetailsActivity extends AppCompatActivity {
     private static final String EXTRA_NAME = "Name";
     private static final String EXTRA_DRINK_TYPE = "drinkType";
-    private static final String EXTRA_ADDITIVES = "additives";
     private static final String EXTRA_DRINK = "drink";
+    private static final String EXTRA_ADDITIVES = "additives";
+
+    private TextView tvName;
+    private TextView tvDrink;
+    private TextView tvDrinkType;
+    private TextView tvAdditivies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_details);
+
+        initViews();
+
+        setValues();
+    }
+
+    private void setValues() {
+        Intent intent = getIntent();
+        tvName.setText(intent.getStringExtra(EXTRA_NAME));
+        tvDrink.setText(intent.getStringExtra(EXTRA_DRINK));
+        tvDrinkType.setText(intent.getStringExtra(EXTRA_DRINK_TYPE));
+        tvAdditivies.setText(intent.getStringArrayListExtra(EXTRA_ADDITIVES).toString());
+    }
+
+    private void initViews() {
+        tvName = findViewById(R.id.tvName);
+        tvDrink = findViewById(R.id.tvDrink);
+        tvDrinkType = findViewById(R.id.tvDrinkType);
+        tvAdditivies = findViewById(R.id.tvAdditivies);
     }
 
     public static Intent newIntent(Context context, String name, String drinkType, ArrayList<String> additives, String drink) {
